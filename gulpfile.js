7dc2bdd716a75143c3f9581f7ceb38dce33fc23b
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-01-02 18:06:06 -0800

    Add config

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const { src, dest, watch, parallel } = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+
+const base = '2-Build_a_Survey_Form' + '/**/*.pug';
+const dist = '2-Build_a_Survey_Form' + '/dist/';
+
+const browsersync = () => {
+  browserSync.init({
+    server: {
+      baseDir: dist,
+    },
+    port: 3000,
+    notify: false,
+  });
+};
+
+const pughtml = () => {
+  return src(base)
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(dest(dist))
+    .pipe(browserSync.stream());
+};
+
+const watching = () => {
+  watch(base, pughtml).on('change', browserSync.reload);
+};
+
+exports.browsersync = browsersync;
+exports.pughtml = pughtml;
+exports.watching = watching;
+
+exports.default = parallel(pughtml, browsersync, watching);
